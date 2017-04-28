c02037e6fe303124cd9912fc547d4e9150b2aecb
Artem L <lavrenov@novikom.ru>
2017-04-28 16:51:09 +0300

    comment for python3 for hello.py

--- hello.py
+++ hello.py
@@ -5,3 +5,14 @@ def app(environ, start_response):
         data.append(line+"\n")
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return data
+
+
+"""
+#!/usr/bin/python                                                                             
+def app(environ, start_response):                                                             
+    data = ''                                                                                 
+    for line in environ["QUERY_STRING"].split("&"):                                           
+        data = data + line + '\n'                                                             
+    start_response('200 OK', [('Content-Type', 'text/plain')])                                
+    return [data.encode()]
+"""
