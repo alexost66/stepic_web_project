3276ea7a7e528d315f2bdd917ab4959dacaf02fe
Artem L <lavrenov@novikom.ru>
2017-05-18 16:55:47 +0300

    myql work

--- init_mysql.sh
+++ init_mysql.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -p -e "CREATE DATABASE qadb;"
-mysql -uroot -p -e "CREATE USER 'django@localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -p -e "GRANT ALL ON qadb.* TO 'django@localhost';"
+mysql -uroot -p -e "CREATE USER 'django' IDENTIFIED BY 'pass123';"
+mysql -uroot -p -e "GRANT ALL ON qadb.* TO 'django';"
 mysql -uroot -p -e "FLUSH PRIVILEGES;"
 
