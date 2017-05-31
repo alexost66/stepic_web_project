80df58d513c5403a3c6c2fdfdd4dd9ed681a53a9
Artem L <lavrenov@novikom.ru>
2017-05-31 15:31:19 +0300

    fix sqlcmd command

--- init_mysql.sh
+++ init_mysql.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/mysql start
-mysql -uroot -p  "CREATE DATABASE qadb;"
-mysql -uroot -p  "CREATE USER 'django'@'localhost' IDENTIFIED BY 'pass123';"
-mysql -uroot -p  "GRANT ALL ON qadb.* TO 'django'@'localhost';"
-mysql -uroot -p  "FLUSH PRIVILEGES;"
+mysql -uroot -u "CREATE DATABASE qadb;"
+mysql -uroot -u "CREATE USER 'django'@'localhost' IDENTIFIED BY 'pass123';"
+mysql -uroot -u "GRANT ALL ON qadb.* TO 'django'@'localhost';"
+mysql -uroot -u "FLUSH PRIVILEGES;"
 
