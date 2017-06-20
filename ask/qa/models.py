76fbc0c0d3ebdeecc193eaed65fb167b1e1a5093
Lavrenov <registera@lavernov.net>
2017-06-20 15:36:58 +0300

    task 2.5 fix 4 printable query

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class Question(models.Model):
@@ -11,6 +12,12 @@ class Question(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name="q_to_likes")
 
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}/".format(self.id)
+
 
 class Answer(models.Model):
     text = models.TextField(default="")
@@ -18,3 +25,5 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User)
 
+    def __str__(self):
+        return self.text
