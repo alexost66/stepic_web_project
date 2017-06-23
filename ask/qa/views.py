7cea5767be2df7c3fdfdb57042310bcf85b69368
Lavrenov <registera@lavernov.net>
2017-06-23 16:40:19 +0300

    task 2.6 try 5

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.paginator import Paginator
 
 from qa.models import Question, Answer
