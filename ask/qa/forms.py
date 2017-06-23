86a8e049fd81cd61271cc692c4666e901cff7239
Lavrenov <registera@lavernov.net>
2017-06-23 16:55:30 +0300

    Task 2.5 fix 8

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -16,6 +16,14 @@ class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     question = forms.IntegerField(widget=forms.HiddenInput)
 
+    def clean_question(self):
+        question_id = self.cleaned_data['question']
+        try:
+            question = Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            question = None
+        return question
+
     def save(self):
         answer = Answer(**self.cleaned_data)
         answer.save()
